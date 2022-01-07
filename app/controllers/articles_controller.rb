7a55b09b55b332665349b51e775d724eeff927ed
anonmy <yajime6859@veb65.com>
2022-01-07 13:56:33 +0500

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+    @article = current_user.articles.build
+  end
+  
+  def create
+    @article = current_user.articles.build(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def edit
+    @article = current_user.articles.find(params[:id])
+  end
+
+  def update
+    @article = current_user.articles.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @article = current_user.articles.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:user_id, :Tittle, :description)
+    end
+
+end
+
+
